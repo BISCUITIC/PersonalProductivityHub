a7f356cef1c60fa8c191f3e306801c8340930dbf
BISCUITIC <tort24457@gamil.com>
2026-03-18 11:04:42 +0300

    Add mapping for Project to ProjectResponse

---
+++ backend/PersonalProductivityHub/Mappings/ProjectExtension.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using PersonalProductivityHub.Contracts.Project;
+
+namespace PersonalProductivityHub.Mappings;
+
+public static class ProjectExtension
+{
+    public static ProjectResponse ToProjectResponse(this Project project)
+    {
+        return new ProjectResponse(project.Id, 
+                                   project.Name, 
+                                   project.Description, 
+                                   project.CreatedAt);
+    }
+}
