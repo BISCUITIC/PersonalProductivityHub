6fbdfda69044d0b723479ca4a95650ff786dfca2
BISCUITIC <tort24457@gamil.com>
2026-03-21 20:10:51 +0300

    Add Me ednpoint. Add mapper from ApplicationUser to AuthResponse

---
+++ backend/PersonalProductivityHub/Mappings/ApplicationUserExtension.cs
@@ -0,0 +1,12 @@
+﻿using Infrastructure.Identity;
+using PersonalProductivityHub.Contracts.Auth;
+
+namespace PersonalProductivityHub.Mappings;
+
+public static class ApplicationUserExtension
+{
+    public static AuthResponse ToAuthResponse(this ApplicationUser user)
+    {
+        return new AuthResponse(user.UserName ?? "", user.Email ?? "", user.CreatedAt);
+    }
+}
